aa7d91a39c05d28801df174983a83a0b76f96f1d
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-11-15 10:15:16 +0700

    chore: add loading component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import { AppProps } from "next/app";
+import cookie from "cookie";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
+}
+export function getServerSideProps(context: any) {
+  const cookies = cookie.parse(context.req.headers.cookie || "");
+  const token = cookies.token;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
 }
---
+++ src/pages/components/atomic/Loading.tsx
@@ -0,0 +1,12 @@
+function Loading() {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="flex flex-col space-y-3 items-center justify-center py-4">
+        <div className="h-10 w-10 animate-spin rounded-full border-t-4 border-purple-500"></div>
+        <p className="font-semibold text-white">Loading</p>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
